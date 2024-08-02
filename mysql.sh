d31899cdca133aa3adb0348ce3c71d93e318d0a3
hemanthdivvela <divvelahemanth7@gmail.com>
2024-08-02 17:35:16 +0530

    update

--- mysql.sh
+++ mysql.sh
@@ -29,23 +29,4 @@ else
 fi
 
 dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Server"
-
-systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
-
-systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
-
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
-
-#Below code will be useful for idempotent nature
-# mysql -h db.daws78s.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
-# if [ $? -ne 0 ]
-# then
-#     mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-#     VALIDATE $? "MySQL Root password Setup"
-# else
-#     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
-#fi
+VALIDATA $? "Installing mysql"server"
